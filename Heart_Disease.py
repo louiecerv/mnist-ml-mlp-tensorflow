fd7a90187c80d61fc03f00d9e1f78c3127c35530
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-29 23:55:59 +0800

    Save

--- Heart_Disease.py
+++ Heart_Disease.py
@@ -20,9 +20,6 @@ def app():
     if "scaler" not in st.session_state:
         st.session_state["scaler"] = StandardScaler()
 
-    if "clf" not in st.session_state:
-        st.session_state.clf = []
-
     if "X_train" not in st.session_state:
         st.session_state.X_train = []
 
@@ -38,8 +35,8 @@ def app():
     if "X_test_scaled" not in st.session_state:
             st.session_state.X_test_scaled = []
 
-    if "n_clusters" not in st.session_state:
-        st.session_state.n_clusters = 4
+    if "dataset_ready" not in st.session_state:
+        st.session_state.dataset_ready = False 
 
     text = """Multi-Layer Perceptron Classifier on the Heart Disease Dataset"""
     st.subheader(text)
@@ -99,8 +96,105 @@ def app():
 
     st.write(text)
 
+    text = """This data set dates from 1988 and consists of four databases: 
+    Cleveland, Hungary, Switzerland, and Long Beach V. It contains 76 attributes, 
+    including the predicted attribute, but all published experiments 
+    refer to using a subset of 14 of them. The "target" field refers to the 
+    presence of heart disease in the patient. It is integer valued 0 = no 
+    disease and 1 = disease.
+    \nAttribute Information:
 
+    age
+    sex
+    chest pain type (4 values)
+    resting blood pressure
+    serum cholestoral in mg/dl
+    fasting blood sugar > 120 mg/dl
+    resting electrocardiographic results (values 0,1,2)
+    maximum heart rate achieved
+    exercise induced angina
+    oldpeak = ST depression induced by exercise relative to rest
+    the slope of the peak exercise ST segment
+    number of major vessels (0-3) colored by flourosopy
+    thal: 0 = normal; 1 = fixed defect; 2 = reversable defect
+    """
+    st.write(text)
+    # Load the data dataset
+    df = pd.read_csv('heart.csv', header=0)
+
+    st.write('Browse the Dataset')
+    st.write(df)
+
+     # Get column names and unique values
+    columns = df.columns
+    unique_values = {col: df[col].unique() for col in columns}    
     
+    # Display unique values for each column
+    st.write("\n**Unique Values:**")
+    for col, values in unique_values.items():
+        st.write(f"- {col}: {', '.join(map(str, values))}")
+
+    st.write('Descriptive Statistics')
+    st.write(df.describe().T)
+
+    # Separate features and target variable
+    X = df.drop('target', axis=1)  # Target variable column name
+    y = df['target']
+
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # store for later use
+    st.session_state.X_train = X_train
+    st.session_state.X_test = X_test
+    st.session_state.y_train = y_train
+    st.session_state.y_test = y_test
+
+    # Standardize features using StandardScaler (recommended)
+    scaler = st.session_state["scaler"] 
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    st.session_state.X_test_scaled = X_test_scaled
+
+    # save the scaler object for later use
+    st.session_state["scaler"] = scaler
+
+    if st.button("Show Graphs"):
+        bins = [10, 30, 50, 70, 90]
+        labels = ['10-29', '30-49', '50-69', '70-89']
+        df['age_group'] = pd.cut(df['age'], bins, labels=labels)
+        countplot(df, "age_group", "sex", "Age and Sex")
+        countplot(df, "sex", "target", "Sex and Heart Disease")
+        countplot(df, "age_group", "target", "Age Group and Heart Disease")
+        countplot(df, "target", "fbs", "FBS>120 and Heart Disease")
+        countplot(df, "target", "thal", "THAL and Heart Disease")
+        plot_feature(df["trestbps"], df["chol"], 'trestbps', 'chol', 'trestbps VS chol')
+        plot_feature(df["thalach"], df["chol"], 'thalach', 'chol', 'thalach VS chol')
+
+def countplot(df, feature, grouping, title):
+    fig, ax = plt.subplots(figsize=(6, 3))
+    # Create the countplot with clear title and legend
+    p = sns.countplot(x=feature, data = df, hue=grouping,  palette='bright')
+    ax.set_title(title, fontsize=14)
+
+    # Display the plot
+    plt.tight_layout()  # Prevent overlapping elements
+    st.pyplot(fig)
+
+
+def plot_feature(feature, target, labelx, labely, title):
+    # Display the plots
+    fig, ax = plt.subplots(figsize=(10, 6))
+    # Scatter plot
+    ax.scatter(feature, target)
+    # Add labels and title
+    ax.set_xlabel(labelx)
+    ax.set_ylabel(labely)
+    ax.set_title(title)
+    # Add grid
+    ax.grid(True)
+    st.pyplot(fig)
+
 #run the app
 if __name__ == "__main__":
     app()
---
+++ pages/1MLP Classifier.py
@@ -0,0 +1,163 @@
+#Input the relevant libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.neural_network import MLPClassifier
+import time
+
+# Define the Streamlit app
+def app():
+    if "dataset_ready" not in st.session_state:
+        st.error("Dataset must be loaded. Click Heart Disease in the sidebar.")
+
+   # Define MLP parameters    
+    st.sidebar.subheader('Set the MLP Parameters')
+    options = ["relu", "tanh", "logistic"]
+    activation = st.sidebar.selectbox('Select the activation function:', options)
+
+    options = ["adam", "lbfgs", "sgd"]
+    solver = st.sidebar.selectbox('Select the solver:', options)
+
+    hidden_layers = st.sidebar.slider(      
+        label="How many hidden layers? :",
+        min_value=5,
+        max_value=250,
+        value=10,  # Initial value
+        step=5
+    )
+
+    alpha = st.sidebar.slider(   
+        label="Set the alpha:",
+        min_value=.001,
+        max_value=1.0,
+        value=0.1,  # In1.0itial value
+    )
+
+    max_iter = st.sidebar.slider(   
+        label="Set the max iterations:",
+        min_value=100,
+        max_value=300,
+        value=120,  
+        step=10
+    )
+
+    # Define the MLP regressor model
+    clf = MLPClassifier(hidden_layer_sizes=(hidden_layers,), 
+            solver=solver, activation=activation, 
+            max_iter=max_iter, random_state=42)
+
+    #store the clf object for later use
+    st.session_state.clf = clf
+
+    text = """Recommended ANN parameters: solver=lbfgs, activation=relu, n_hidden_layer=150, max_iter=150"""
+    st.write(text)
+    if st.button('Start Training'):
+        progress_bar = st.progress(0, text="Training the MLP regressor can take up to five minutes please wait...")
+
+        # Train the model 
+        train_model(X_train_scaled, y_train)
+
+        # update the progress bar
+        for i in range(100):
+            # Update progress bar value
+            progress_bar.progress(i + 1)
+            # Simulate some time-consuming task (e.g., sleep)
+            time.sleep(0.01)
+        # Progress bar reaches 100% after the loop completes
+        st.success("Regressor training completed!") 
+
+    st.subheader('Performance of the MLP-ANN Classifier on the Heart Disease Dataset')
+    text = """We test the performance of the MLP Classifer using the 20% of the dataset that was
+    set aside for testing. The confusion matrix and classification report are presented below."""
+    st.write(text)
+    
+    if st.button('Begin Test'):
+        progress_bar = st.progress(0, text="Performance test has started please wait...")
+
+        X_test_scaled = st.session_state.X_test_scaled
+        # Make predictions on the test set
+        y_test_pred = st.session_state.clf.predict(X_test_scaled)
+        y_test = st.session_state.y_test
+
+        # update the progress bar
+        for i in range(100):
+            # Update progress bar value
+            progress_bar.progress(i + 1)
+            # Simulate some time-consuming task (e.g., sleep)
+            time.sleep(0.01)
+        # Progress bar reaches 100% after the loop completes
+        st.success("Performance test completed!") 
+        
+        st.subheader('Confusion Matrix')
+
+        st.write('Confusion Matrix')
+        cm = confusion_matrix(y_test, y_test_pred)
+        st.text(cm)
+
+        text = """The confusion matrix shows the performance of an MLP (Multi-Layer Perceptron) 
+        classifier on a heart disease prediction task, classifying individuals as either 
+        having or not having heart disease. Let's break down the results and their 
+        implications for future unseen data:
+        Cells (These numbers were produced by a run using certain parameters of the MLP.  Other 
+        combinations of parameters could produce different values.):
+        [79, 23]: This cell represents individuals correctly classified as healthy 
+        (negative). There were 79 true negatives (TN).
+        [11, 92]: This cell represents individuals with heart disease. The model 
+        correctly classified 92 (true positives - TP) and misclassified 11 (false positives - FP) as healthy.
+        Implications:
+        Overall Accuracy: By adding TN and TP (79 + 92) and dividing by the total (205), 
+        we get a baseline accuracy of (79 + 92) / 205 = 84.4%. This indicates the model 
+        performs decently overall.
+        False Positives (FP): The model incorrectly classified 11 individuals with heart 
+        disease as healthy. This could be concerning in a heart disease prediction 
+        scenario. A false positive might lead to someone with heart disease not 
+        receiving proper treatment.
+        True Negatives (TN): The model successfully identified 79 healthy individuals. 
+        This is a positive aspect, meaning the model can avoid unnecessary interventions 
+        for healthy people.
+        Unseen Data: The accuracy on unseen data might be similar but can not be guaranteed. 
+        The model's performance is based on the data it was trained on. Generalizability to 
+        unseen data is a challenge in machine learning, and real-world data can deviate 
+        from the training data in unforeseen ways.
+        Overall: The model shows promise, but the false positives require further investigation.  
+        Depending on the application,  mitigating these  false positives might be crucial.  
+        It's important to consider additional metrics  like True Negatives Rate 
+        (specificity) and False Positive Rate  (specificity) for a more comprehensive evaluation."""
+        st.write(text)
+        st.subheader('Performance Metrics')
+        st.text(classification_report(y_test, y_test_pred))
+  
+        text = """An accuracy of more than 80% on the heart disease dataset for the MLP classifier 
+        indicates that the model performs well on the data it was trained on. It can correctly 
+        identify 80% of the data points as having or not having heart disease based on the features 
+        it was trained on.
+        However, there are limitations to consider when interpreting this accuracy:
+        Overfitting: The model might have memorized the training data too well and may not generalize
+        well to unseen data. This means it might not perform as well on new heart disease cases it 
+        has never encountered before.
+        Data Representativeness: The accuracy reflects how well the model performs on the specific 
+        dataset it was trained on. If the dataset doesn't represent the real-world distribution of 
+        heart disease cases well, the 83% accuracy might not be reliable for real-world application.
+        Therefore, based solely on the 83% accuracy, it's not possible to reliably predict unseen 
+        heart disease data. 
+        \nAccuracy is just one measure of performance. Consider metrics like precision, 
+        recall, and F1 score to understand how well the model performs on different
+        types of classification errors (false positives and false negatives).
+        Domain knowledge: In the medical field, even a small misclassification 
+        can have serious consequences. 
+        \nConsult medical experts to understand the acceptable level of error 
+        for this application."""
+        
+        with st.expander('Click to view more details.'):
+            st.write(text)
+
+def train_model(X_train_scaled, y_train):
+    clf = st.session_state.clf 
+    clf.fit(X_train_scaled, y_train)
+    st.session_state.clf = clf
+
+#run the app
+if __name__ == "__main__":
+    app()
