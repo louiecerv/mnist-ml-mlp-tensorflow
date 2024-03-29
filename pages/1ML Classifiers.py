836924f1c172a98c3fae4c91c458727f0ef1cc25
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-30 00:35:42 +0800

    Save

---
+++ pages/1ML Classifiers.py
@@ -0,0 +1,16 @@
+#Input the relevant libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.neural_network import MLPClassifier
+import time
+
+# Define the Streamlit app
+def app():
+    st.write("Replace this placeholder.")
+
+#run the app
+if __name__ == "__main__":
+    app()
