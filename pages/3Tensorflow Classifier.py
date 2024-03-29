d74ba9c3a9219944609d7c94d0b05b102b26a0c4
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-30 01:04:28 +0800

    Save

--- pages/3Tensorflow Classifier.py
+++ pages/3Tensorflow Classifier.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import tensorflow
 from tensorflow import keras
 from tensorflow.keras import layers
 
